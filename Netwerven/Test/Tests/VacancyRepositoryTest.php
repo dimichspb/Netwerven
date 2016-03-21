33c5e890febabf96fbb56359a40afd1737ccebde
Dmitry Tarantin <dimichspb@gmail.com>
2016-03-21 15:10:06 +0300

    Some changes in tests

--- Netwerven/Test/Tests/VacancyRepositoryTest.php
+++ Netwerven/Test/Tests/VacancyRepositoryTest.php
@@ -10,6 +10,8 @@ class VacancyRepositoryTest extends \PHPUnit_Framework_TestCase
 
     const TESTING_DATASOURCE_ALIAS = 'testingDataSource';
     const TESTING_DATASOURCE_CLASS = 'Netwerven\Test\DataSources\DataSource';
+    const TESTING_JSON_FILENAME = 'temp.json';
+    const TESTING_JSON_PRIMARYKEY = 'id';
 
     public $testingDataSource;
     public $testingDataSourceConfig;
@@ -19,8 +21,8 @@ class VacancyRepositoryTest extends \PHPUnit_Framework_TestCase
         require_once (dirname(__DIR__) . DIRECTORY_SEPARATOR . "Bootstrap.php");
 
         $this->testingDataSourceConfig = new JsonConfig([
-            'filePath' => 'temp.json',
-            'primaryKey' => 'id',
+            'filePath' => self::TESTING_JSON_FILENAME,
+            'primaryKey' => self::TESTING_JSON_PRIMARYKEY,
             'mapping' => [],
         ]);
         $this->testingDataSource = new JsonDataSource($this->testingDataSourceConfig);
@@ -79,7 +81,9 @@ class VacancyRepositoryTest extends \PHPUnit_Framework_TestCase
     public function testActivate()
     {
         TestingRepository::activate(self::TESTING_DATASOURCE_ALIAS);
+        var_dump(TestingRepository::source(self::TESTING_DATASOURCE_ALIAS));
         $result = TestingRepository::isActive(self::TESTING_DATASOURCE_ALIAS);
+        var_dump($result);
         $this->assertTrue($result);
     }
 
